babb4c255bbc987f0d620d45e2b34180ca0d9c4d
maripiero23 <mari.pieropan@gmail.com>
2023-01-20 17:49:29 +0100

    qualcosina

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        
+        <div class="container pt-5">
+            <div class="row">
+                <div class="col-6">
+                    <div class="row d-flex gap-3">
+                        <div class="col-3 text-white">
+                            <div class="title-footer">
+                                <h5 class="fs-semibold text-white">DC COMICS</h5>
+                            </div>
+                            <ul class="links-footer list-unstyled">
+                                <li class="text-secondary"><small>CHARACTERS</small><a href=""></a></li>
+                                <li class="text-secondary"><small>COMICS</small><a href=""></a></li>
+                                <li class="text-secondary"><small>MOVIES</small><a href=""></a></li>
+                                <li class="text-secondary"><small>TV</small><a href=""></a></li>
+                                <li class="text-secondary"><small>GAMES</small><a href=""></a></li>
+                                <li class="text-secondary"><small>VIDEOS</small><a href=""></a></li>
+                                <li class="text-secondary"><small>NEWS</small><a href=""></a></li>
+                            </ul>
+
+                            <div class="title-footer">
+                                <h5 class="fs-semibold text-white">SHOP</h5>
+                            </div>
+                            <ul class="links-footer list-unstyled">
+                                <li class="text-secondary"><small>SHOP DC</small><a href=""></a></li>
+                                <li class="text-secondary"><small>SHOP DC Collectibles</small><a href=""></a></li>
+
+                            </ul>
+                        </div>
+
+                        <div class="col-3 text-white">
+                            <div class="title-footer">
+                                <h5 class="fs-semibold text-white">DC</h5>
+                            </div>
+                            <ul class="links-footer list-unstyled">
+                                <li class="text-secondary"><small>CHARACTERS</small><a href=""></a></li>
+                                <li class="text-secondary"><small>COMICS</small><a href=""></a></li>
+                                <li class="text-secondary"><small>MOVIES</small><a href=""></a></li>
+                                <li class="text-secondary"><small>TV</small><a href=""></a></li>
+                                <li class="text-secondary"><small>GAMES</small><a href=""></a></li>
+                                <li class="text-secondary"><small>VIDEOS</small><a href=""></a></li>
+                                <li class="text-secondary"><small>NEWS</small><a href=""></a></li>
+                                <li class="text-secondary"><small>SHOP DC</small><a href=""></a></li>
+                                <li class="text-secondary"><small>SHOP DC Collectibles</small><a href=""></a></li>
+                            </ul>
+
+                        </div>
+                        <div class="col-3 text-white">
+                            <div class="title-footer">
+                                <h5 class="fs-semibold text-white">SITES</h5>
+                            </div>
+                            <ul class="links-footer list-unstyled">
+                                <li class="text-secondary"><small>DC</small><a href=""></a></li>
+                                <li class="text-secondary"><small>MAD Magazine</small><a href=""></a></li>
+                                <li class="text-secondary"><small>DC Kids</small><a href=""></a></li>
+                                <li class="text-secondary"><small>DC Universe</small><a href=""></a></li>
+                                <li class="text-secondary"><small>DC Power Visa</small><a href=""></a></li>
+    
+                            </ul>
+                        </div>
+                    </div>
+    
+                </div>
+    
+                <div class="col-6">
+                    <div class="logone">
+                        <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+                    </div>
+    
+                </div>
+    
+            </div>
+        </div>
+
+        <section class="sign-icons-section">
+            <div class="container butt-icons-flex">
+                <div class="sign-up">
+                    <button type="button" class="btn btn-outline-primary rounded-0 text-white px-2 fw-semibold">SIGN-UP NOW!</button>
+                </div>
+            
+                <div class="follow-icons-container">
+                    <span class="text-primary fw-semibold">FOLLOW US</span>
+                    <img class="icons" src="" alt="">
+                    <img class="icons" src="../assets/img/footer-facebook.png" alt="">
+                    <img class="icons" src="../assets/img/footer-youtube.png" alt="">
+                    <img class="icons" src="../assets/img/footer-pinterest.png" alt="">
+                    <img class="icons" src="../assets/img/footer-periscope.png" alt="">
+    
+                </div>
+            </div>
+    
+            
+        </section>
+    </footer>
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <body>
+        <header>
+            <nav class="navbar fixed-top d-flex">
+                <div class="container">
+                    <div class="dc-logo">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                    </div>
+                    <div class="">
+                        <ul class="navbar nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">CHARACTERS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">COMICS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">MOVIES</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">TV</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">GAMES</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">COLLECTIBLES</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">VIDEOS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">FAN</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">NEWS</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SHOP</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="search">
+                            <input type="search" placeholder="Search" class="rounded">
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </body>
+</html>
+
